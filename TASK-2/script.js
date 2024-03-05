ee85787814e91b54cfee23a5f13b6079d5703b84
Mainak0220 <114420982+Mainak0220@users.noreply.github.com>
2024-03-05 17:03:27 +0530

    Add files via upload

---
+++ TASK-2/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings:["a Frontend Developer","an Artist","a Youtuber"],
+    typespeed:40,
+    backspeed:40,
+    backDelay:1000,
+    loop:true
+});
